6ade8c79185d3b31072132f632951d66c4b6e3d6
sakovalchuk99 <sakovalchuk99@gmail.com>
2018-12-10 23:45:49 +0200

    text

---
+++ Lab08Lib/Sub.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab08Lib
+{
+    public class Sub : IComparable
+    {
+        public string Name { get; set; }
+        public string SecondName { get; set; }
+        public string PhoneNum { get; set; }
+        public string Country { get; set; }
+        private int cost;
+        public int Cost
+        {
+            get
+            { return cost; }
+            set
+            {
+                if (value > 1 && value < 300)
+                {
+                    cost = value;
+                }
+                else
+                {
+                    throw new InvalidCostException();
+                }
+            }
+        }
+        public bool HasCost { get; set; }
+
+        public Sub()
+        {
+
+        }
+        public Sub(string name, string secondName, string phoneNum, string country, int cost, bool hasCost)
+        {
+            Name = name;
+            SecondName = secondName;
+            PhoneNum = phoneNum;
+            Country = country;
+            Cost = cost;
+            HasCost = hasCost;
+        }
+        public string Info()
+        {
+            return Name + ", " + SecondName + ", " + Country+ ", "+PhoneNum+", " +Cost;
+        }
+        public int CompareTo(object obj)
+        {
+            Sub t = obj as Sub;
+            return string.Compare(this.Name, t.Name);
+
+        }
+    }
+}
---
+++ Lab14/MainCode.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lab08Lib;
+using System.IO;
+
+namespace Lab14
+{
+    class MainCode
+    {
+        static List<Sub> subs;
+        static void PrintSubs()
+        {
+            foreach (Sub sub in subs)
+            {
+                Console.WriteLine(sub.Info().Replace('і', 'i'));
+            }
+            Console.WriteLine(); }
+        static void Main(string[] args)
+        {
+            subs = new List<Sub>();
+            FileStream fs = new FileStream("TEST.subs", FileMode.Open);
+            BinaryReader reader = new BinaryReader(fs);
+            try
+            {
+                Sub sub;
+                
+                while (reader.BaseStream.Position < reader.BaseStream.Length)
+                {
+                    sub = new Sub();
+                    for (int i = 1; i <= 8; i++)
+                    {
+                        switch (i)
+                        {
+                            case 1:
+                                sub.Name = reader.ReadString();
+                                break;
+
+                            case 2:
+                                sub.SecondName = reader.ReadString();
+                                break;
+
+                            case 3:
+                                sub.PhoneNum = reader.ReadString();
+                                break;
+
+                            case 4:
+                                sub.Country = reader.ReadString();
+                                break;
+
+                            case 5:
+                                sub.Cost = reader.ReadInt32();
+                                break;
+
+                            case 6:
+                                sub.HasCost = reader.ReadBoolean();
+                                break;
+                        }
+                    }
+                    subs.Add(sub);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Сталась помилка: {0}", ex.Message);
+            }
+            finally
+            {
+                reader.Close();
+            }
+            Console.WriteLine("    Не отсортированый список: {0}", subs.Count);
+            PrintSubs();
+            subs.Sort();
+            Console.WriteLine("    Отсортированый список: {0}", subs.Count);
+            PrintSubs();
+            Console.WriteLine("    Добавляем нового абонента: Гриша");
+            Sub subNikita = new Sub("Гриша", "Чумачук", "(095)092-3292", "Украина", 175, true);
+            subs.Add(subNikita);
+            subs.Sort();
+            Console.WriteLine("    Список абонентов: {0}", subs.Count);
+            PrintSubs();
+            Console.WriteLine("    Удаляем последнего абонента:");
+            subs.RemoveAt(subs.Count - 1);
+            Console.WriteLine("    Обновленный список: {0}", subs.Count);
+            PrintSubs();
+            Console.ReadKey();
+
+        }
+    }
+ }
+
+
